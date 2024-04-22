ac893f03d951a4b6c5a6bd2d512e4c8ca6c32662
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-04-22 23:44:59 +0900

    4/22

---
+++ src/main/java/CenterApple/H2/domain/CenterController.java
@@ -0,0 +1,11 @@
+package CenterApple.H2.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/applehome")
+public class CenterController {
+
+
+}
