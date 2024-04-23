ef1ac0fb4325c94b5af59278256e411e8cf7f51f
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-04-23 18:42:18 +0900

    4/23

--- src/main/java/CenterApple/H2/domain/CenterController.java
+++ src/main/java/CenterApple/H2/domain/CenterController.java
@@ -1,11 +1,13 @@
 package CenterApple.H2.domain;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/applehome")
 public class CenterController {
 
+    @PostMapping
 
 }
